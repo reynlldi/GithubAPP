886325378ae2abd8b926662bf2bf6e373631920e
Reynlldi <reynalddi93@gmail.com>
2023-10-30 22:21:00 +0700

    memperbaiki headers token

--- app/src/main/java/com/example/github/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/example/github/data/retrofit/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.example.github.data.retrofit
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,10 +9,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
-            val loggingInterceptor =
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val authInterceptor = Interceptor{ chain ->
+                val req = chain.request()
+                val requestHeaders = req.newBuilder()
+                    .addHeader("Authorization", "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")
+                    .build()
+                chain.proceed(requestHeaders)
+            }
             val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
+                .addInterceptor(authInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
